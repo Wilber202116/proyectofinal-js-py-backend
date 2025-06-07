8ba87cf789e0942c8098f56cb7849c02eb0b6514
Wilber202116 <157905431+Wilber202116@users.noreply.github.com>
2025-06-06 20:30:00 -0600

    Update config.py
    change data_folder

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    DATA_FOLDER = './backend/data'
+    DATA_FOLDER = './data'
     DATABASE_FILE = os.path.join(DATA_FOLDER, 'database.json')
     SQLITE_DB = os.path.join(DATA_FOLDER, 'users.db') # ./data/users.db
